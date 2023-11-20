8f76f7204d3e4375ffbecd463f7f854bba46ee6e
bright123-strive <brimuwemi@gmail.com>
2023-11-20 13:17:00 +0200

    dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -6,13 +6,34 @@ use Livewire\Component;
 use App\Models\User;
 use App\Models\Subscription;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 
 class Dashboard extends Component
 {
     public function render()
     {
+
+
+        $loggedInUser = Auth::user();
+
+        // Count active subscriptions
+        $userActiveSubscriptionsCount = Subscription::where('status', 'active')
+            ->where('user_id', $loggedInUser->id)
+            ->count();
+
+        // Count expired subscriptions
+        $expiredSubscriptionsCount = Subscription::where('status', 'expired')
+            ->where('user_id', $loggedInUser->id)
+            ->count();
+
+        // Count not activated subscriptions
+        $notActivatedSubscriptionsCount = Subscription::where('status', 'inactive')
+            ->where('user_id', $loggedInUser->id)
+            ->count();
+
         $subscribersCount = User::where('role_id', 3)->count();
+
         $activeSubscriptionsCount = Subscription::where('status', 'active')->count();
         $usersCount = User::whereIn('role_id', [1, 2])->count();
 
@@ -21,13 +42,17 @@ class Dashboard extends Component
 
        $subscriptionsCount = Subscription::whereDate('created_at', $today)->count();
 
-       dd($subscriptionsCount);
+
 
 
         return view('livewire.dashboard',[
             'subscribers' =>$subscribersCount,
             'activeSubscriptions' =>$activeSubscriptionsCount,
             'stuffs' =>$usersCount,
+            'todaysSubscription' =>$subscriptionsCount,
+            'userActiveSubscriptions' =>$userActiveSubscriptionsCount,
+            'userInActiveSubscriptions' =>$notActivatedSubscriptionsCount,
+            'userExpiredSubscriptions' =>$expiredSubscriptionsCount,
 
         ]);
     }
